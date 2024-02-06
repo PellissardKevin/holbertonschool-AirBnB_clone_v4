7d9819ea51b1cacfe83f42eae285c3387f0accea
Kevin.P <kevin.pellissard@gmail.com>
2024-02-06 16:01:18 +0100

    Update all task with food html page, and good filters

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -23,11 +23,17 @@ $(document).ready(function () {
     updateAmenityList();
   });
 
-  $.get("http://0.0.0.0:5001/api/v1/status/", function (data) {
-    if (data.status === "OK") {
-      $("#api_status").addClass("available");
-    } else {
-      $("#api_status").removeClass("available");
-    }
+  $.ajax({
+      url: 'http://127.0.0.1:5001/api/v1/status/',
+      success(response) {
+          if (response.status === 'OK') {
+              $('#api_status').addClass('available');
+          } else {
+              $('#api_status').removeClass('available');
+          }
+      },
+      error(error) {
+          console.error('Error fetching API status', error);
+      },
   });
 });
--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -24,51 +24,39 @@ $(document).ready(function () {
   });
 
   $.ajax({
-    type: "POST",
-    url: "http://0.0.0.0:5001/api/v1/places_search",
-    contentType: "application/json",
+    url: 'http://127.0.0.1:5001/api/v1/places_search/',
+    type: 'POST',
     data: JSON.stringify({}),
-    success: function (data) {
-      data.forEach(function (place) {
-        const article = $("<article>").addClass("place");
-        const name = $("<h2>").text(place.name);
-        const price = $("<div>")
-          .addClass("price_by_night")
-          .text("$" + place.price_by_night);
-        const description = $("<div>")
-          .addClass("description")
-          .text(place.description);
-        const guest = $("<div>")
-          .addClass("max_guest")
-          .text(place.max_guest + " Guest(s)");
-        const rooms = $("<div>")
-          .addClass("number_rooms")
-          .text(place.number_rooms + " Room(s)");
-        const bathrooms = $("<div>")
-          .addClass("number_bathrooms")
-          .text(place.number_bathrooms + " Bathroom(s)");
-        const amenities = $("<div>").addClass("amenities");
-
-        place.amenities.forEach(function (amenity) {
-          const amenityDiv = $("<div>").addClass("amenity").text(amenity);
-          amenities.append(amenityDiv);
-        });
-
-        article.append(
-          name,
-          price,
-          description,
-          guest,
-          rooms,
-          bathrooms,
-          amenities
-        );
-        $(".places").append(article);
-      });
-    },
+    headers: { 'Content-Type': 'application/json' },
+    dataType: 'json',
+    success: function (response) {
+      for (let i = 0; i < response.length; i++) {
+        $('.places').append('<article></article>');
+        $('article:last').append('<div></div>');
+        $('article:last div').addClass('title_box');
+        $('.title_box:last').append('<h2>' + response[i].name + '</h2>');
+        $('.title_box:last').append('<div></div>');
+        $('.title_box:last div').addClass('price_by_night');
+        $('.price_by_night:last').append('$' + response[i].price_by_night);
+        $('article:last').append('<div></div>');
+        $('article:last div:last').addClass('information');
+        $('.information:last').append('<div></div>');
+        $('.information:last div').addClass('max_guest');
+        $('.max_guest:last').append(response[i].max_guest + ' Guest');
+        $('.information:last').append('<div></div>');
+        $('.information:last div:last').addClass('number_rooms');
+        $('.number_rooms:last').append(response[i].number_rooms + ' Bedroom');
+        $('.information:last').append('<div></div>');
+        $('.information:last div:last').addClass('number_bathrooms');
+        $('.number_bathrooms:last').append(response[i].number_bathrooms + ' Bathroom');
+        $('article:last').append('<div></div>');
+        $('article:last div:last').addClass('description');
+        $('.description:last').append(response[i].description);
+      }
+    }
   });
 
-  $.get("http://0.0.0.0:5001/api/v1/status/", function (data) {
+  $.get("http://127.0.0.1:5001/api/v1/status/", function (data) {
     if (data.status === "OK") {
       $("#api_status").addClass("available");
     } else {
