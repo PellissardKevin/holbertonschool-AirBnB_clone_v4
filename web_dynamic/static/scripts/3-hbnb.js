45600fc0c1c5933fc42392aae405a920ff603c8b
Kevin.P <kevin.pellissard@gmail.com>
2024-02-06 14:49:58 +0100

    Add task 3 to update the Places with JS et not jinja

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -23,6 +23,51 @@ $(document).ready(function () {
     updateAmenityList();
   });
 
+  $.ajax({
+    type: "POST",
+    url: "http://0.0.0.0:5001/api/v1/places_search",
+    contentType: "application/json",
+    data: JSON.stringify({}),
+    success: function (data) {
+      data.forEach(function (place) {
+        const article = $("<article>").addClass("place");
+        const name = $("<h2>").text(place.name);
+        const price = $("<div>")
+          .addClass("price_by_night")
+          .text("$" + place.price_by_night);
+        const description = $("<div>")
+          .addClass("description")
+          .text(place.description);
+        const guest = $("<div>")
+          .addClass("max_guest")
+          .text(place.max_guest + " Guest(s)");
+        const rooms = $("<div>")
+          .addClass("number_rooms")
+          .text(place.number_rooms + " Room(s)");
+        const bathrooms = $("<div>")
+          .addClass("number_bathrooms")
+          .text(place.number_bathrooms + " Bathroom(s)");
+        const amenities = $("<div>").addClass("amenities");
+
+        place.amenities.forEach(function (amenity) {
+          const amenityDiv = $("<div>").addClass("amenity").text(amenity);
+          amenities.append(amenityDiv);
+        });
+
+        article.append(
+          name,
+          price,
+          description,
+          guest,
+          rooms,
+          bathrooms,
+          amenities
+        );
+        $(".places").append(article);
+      });
+    },
+  });
+
   $.get("http://0.0.0.0:5001/api/v1/status/", function (data) {
     if (data.status === "OK") {
       $("#api_status").addClass("available");
